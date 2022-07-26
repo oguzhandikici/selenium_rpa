5a54585c9e9eccb602f0303f311833723bac191b
oguzhandikici <oguzhandikici97@gmail.com>
2022-07-26 18:17:25 +0300

    diger bedenler

--- zara.py
+++ zara.py
@@ -29,11 +29,31 @@ driver.get(url)
 
 driver.save_screenshot('./ozi_pantolon.png')
 
+# PANTOLON
 us30_beden = driver.find_element(By.ID, 'product-detail-size-selector-product-detail-product-size-selector-153423933-item-0')
 is_available = False if 'size-out-of-stock' == us30_beden.get_attribute('data-qa-action') else True
+us31_beden = driver.find_element(By.ID, 'product-detail-size-selector-product-detail-product-size-selector-153423933-item-1')
+us32_beden = driver.find_element(By.ID, 'product-detail-size-selector-product-detail-product-size-selector-153423933-item-2')
+us34_beden = driver.find_element(By.ID, 'product-detail-size-selector-product-detail-product-size-selector-153423933-item-3')
+us36_beden = driver.find_element(By.ID, 'product-detail-size-selector-product-detail-product-size-selector-153423933-item-4')
+other_sizes = [us31_beden, us32_beden, us34_beden, us36_beden]
+
+is_other_available = False
+for size in other_sizes:
+    if size.get_attribute('data-qa-action') != 'size-out-of-stock':
+        is_other_available = True
+        break
+
+if is_available:
+    send_telegram_message(f'Pantolon Geldi HEM DE 30 BEDEN ALO: {url}', '-596278799')
+elif is_other_available:
+    send_telegram_message(f'Pantolon Geldi AMA BASKA BEDEN: {url}', '-596278799')
+
 
 print(is_available)
 
+
+# BABAANNE TSHIRT
 babaanne_url = "https://www.zara.com/tr/tr/pamuklu-premium-t-shirt-p04174015.html?v1=172577525&v2=2137904"
 driver.get(babaanne_url)
 
@@ -42,10 +62,8 @@ is_babaanne_available = False if 'size-out-of-stock' == babaanne_L.get_attribute
 print(is_babaanne_available)
 driver.save_screenshot('./babaanne_tshirt.png')
 driver.quit()
-send_telegram_message(f'calisiyor', '-770148157')
-
-if is_available:
-    send_telegram_message(f'Pantolon Geldi HEM DE 30 BEDEN ALO: {url}', '-596278799')
 
 if is_babaanne_available:
     send_telegram_message(f'BABAANNE T-SHIRT GELDİ: {babaanne_url}', '-596278799')
+
+send_telegram_message(f'calisiyor', '-770148157')
