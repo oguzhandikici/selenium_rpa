4c5e028bb170672cccd2a02dda54e026e7360d4f
oguzhandikici <oguzhandikici97@gmail.com>
2022-07-29 22:09:28 +0300

    otomasyon oranı arttırıldı

--- zara.py
+++ zara.py
@@ -1,3 +1,4 @@
+from email import message
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
@@ -15,6 +16,17 @@ def send_telegram_message(message, chat_id):
     return result
 
 
+def check_availability_of_cloth_size(driver, url, button_id, message):
+    driver.get(url)
+    size_button = driver.find_element(By.ID, button_id)
+    print(size_button)
+    is_size_available = False if 'size-out-of-stock' == size_button.get_attribute('data-qa-action') else True
+
+    if is_size_available:
+        send_telegram_message(f'{message}: {babaanne_url}', '-596278799')
+    return send_telegram_message
+
+
 options = Options()
 options.add_argument("--window-size=1920,1080")
 options.add_argument('--headless')
@@ -24,46 +36,25 @@ user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Ge
 options.add_argument(f'user-agent={user_agent}')
 
 driver = webdriver.Chrome('./chromedriver.exe', options=options)
-url = "https://www.zara.com/tr/tr/premium-slim-fit-jean-p05072402.html?v1=153423933"
-driver.get(url)
 
-driver.save_screenshot('./ozi_pantolon.png')
+# TAKIM BLAZER
+url = "https://www.zara.com/tr/tr/pamuklu-keten-blazer-takim-p05631150.html?v1=200488403&v2=2111004"
+button_id = "product-detail-size-selector-product-detail-product-size-selector-200488403-item-0"
+message = 'Blazer geldi'
+check_availability_of_cloth_size(driver, url, button_id, message)
 
-# PANTOLON
-us30_beden = driver.find_element(By.ID, 'product-detail-size-selector-product-detail-product-size-selector-153423933-item-0')
-is_available = False if 'size-out-of-stock' == us30_beden.get_attribute('data-qa-action') else True
-us31_beden = driver.find_element(By.ID, 'product-detail-size-selector-product-detail-product-size-selector-153423933-item-1')
-us32_beden = driver.find_element(By.ID, 'product-detail-size-selector-product-detail-product-size-selector-153423933-item-2')
-us34_beden = driver.find_element(By.ID, 'product-detail-size-selector-product-detail-product-size-selector-153423933-item-3')
-us36_beden = driver.find_element(By.ID, 'product-detail-size-selector-product-detail-product-size-selector-153423933-item-4')
-other_sizes = [us31_beden, us32_beden, us34_beden, us36_beden]
-
-is_other_available = False
-for size in other_sizes:
-    if size.get_attribute('data-qa-action') != 'size-out-of-stock':
-        is_other_available = True
-        break
-
-if is_available:
-    send_telegram_message(f'Pantolon Geldi HEM DE 30 BEDEN ALO: {url}', '-596278799')
-elif is_other_available:
-    send_telegram_message(f'Pantolon Geldi AMA BASKA BEDEN: {url}', '-596278799')
-
-
-print(is_available)
-
+# TAKIM PANTOLON
+url = "https://www.zara.com/tr/tr/pamuklu-keten-takim-pantolonu-p05638150.html?v1=200488404&v2=2111004"
+button_id = "product-detail-size-selector-product-detail-product-size-selector-200488404-item-0"
+message = 'Takım pantolon geldi'
+check_availability_of_cloth_size(driver, url, button_id, message)
 
 # BABAANNE TSHIRT
-babaanne_url = "https://www.zara.com/tr/tr/pamuklu-premium-t-shirt-p04174015.html?v1=172577525&v2=2137904"
-driver.get(babaanne_url)
+url = "https://www.zara.com/tr/tr/pamuklu-premium-t-shirt-p04174015.html?v1=172577525&v2=2137904"
+button_id = 'product-detail-size-selector-product-detail-product-size-selector-172577525-item-3'
+message = 'Babaanne tişört geldi'
+check_availability_of_cloth_size(driver, url, button_id, message)
 
-babaanne_L = driver.find_element(By.ID, 'product-detail-size-selector-product-detail-product-size-selector-172577525-item-3')
-is_babaanne_available = False if 'size-out-of-stock' == babaanne_L.get_attribute('data-qa-action') else True
-print(is_babaanne_available)
-driver.save_screenshot('./babaanne_tshirt.png')
 driver.quit()
-
-if is_babaanne_available:
-    send_telegram_message(f'BABAANNE T-SHIRT GELDİ: {babaanne_url}', '-596278799')
 
 send_telegram_message(f'calisiyor', '-770148157')
